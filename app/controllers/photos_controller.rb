4ffbe08ca4c0e3121ccbdff046667f223614e218
Natalie Holton <natalie.k.holton@gmail.com>
2015-12-21 03:36:16 +0000

    fixed some code on photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,12 @@
 class PhotosController < ApplicationController
 
+	def create
+		@photo = current_user.photos.create(photo_params)
+		if @place.valid?
+			redirect_to root_path
+	end
 
-
+	
 	private
 
 	def photo_params
