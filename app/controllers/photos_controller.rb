6a1001718a3ff11d2914233299b0dad0a865269e
Natalie Holton <natalie.k.holton@gmail.com>
2015-12-21 15:12:55 +0000

    made a few more changes to show.html.erb

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,14 @@
 class PhotosController < ApplicationController
 
 	before_action :authenticate_user!
-	
+
 	def show
 		@place = Place.find(params[:id])
 		@photo = Photo.new
 	end
 
 	def create
-		@photo = Photo.find(params[:place_id])
+		@photo = Place.find(params[:place_id])
 		@photo.photos.create(photo_params.merge(:user => current_user))
 		redirect_to place_path(@place)
 	end
