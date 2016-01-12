02d46ec672f4bb69a97200d84fee76516c5cd7c2
Natalie Holton <natalie.k.holton@gmail.com>
2016-01-12 23:52:25 +0000

    fixed all tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -3,23 +3,22 @@ require 'test_helper'
 class CommentsControllerTest < ActionController::TestCase
 
     test "comment creation functionality" do 
+  	
   	user = FactoryGirl.create(:user)
   	sign_in user
 
   	place = FactoryGirl.create(:place)
-  	end
 
   	assert_difference 'Comment.count' do
-  		post :create, :place_id => place.id, {comment: =>{
-  			message: => 'great food',
-  			rating: => '4_stars',
-  			}
+  		post :create, :place_id => place.id, :comment =>{
+  			:message => 'great food',
+  			:rating => '4_stars',
   		}
-  		end
   	end
 
   	assert_redirected_to place_path(place)
 
   	assert_equal 1, user.comments.count
-  end
+
+  	end
 end
