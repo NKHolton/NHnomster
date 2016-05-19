236a0d727cfb214d32cbbc775dbdfe32e0b111e4
Natalie <natalie.k.holton@gmail.com>
2016-05-19 17:41:38 +0000

    updated production.rb to have sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,24 @@
 Nomster::Application.configure do
-    
-    ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'nomster-natalie-holton.herokuapp.com',
+
+  #   ActionMailer::Base.smtp_settings = {
+  #   :port           => ENV['MAILGUN_SMTP_PORT'],
+  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
+  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  #   :domain         => 'nomster-natalie-holton.herokuapp.com',
+  #   :authentication => :plain,
+  # }
+
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-  }
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+    }
   ActionMailer::Base.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { :host => 'nomster-natalie-holton.herokuapp.com' }
